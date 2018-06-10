86aa844d47203ac1be1d4629fa1fc06eeaaff17d
alganbr <algan_br@hotmail.com>
2018-06-09 17:35:21 -0700

    Cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const mongoose = require('mongoose');
 const passport = require('passport');
 const bodyParser = require("body-parser");
